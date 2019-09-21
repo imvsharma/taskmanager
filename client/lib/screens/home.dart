e3dd845b0fd4803293124fcb2c9b26335c063cef
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-09-22 00:55:28 +0530

    customize app bar

---
+++ client/lib/screens/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(200.0),
+        child: AppBar(
+          title: Text('Task Manager'),
+          backgroundColor: const Color(0xFF2C9AB0),
+        ),
+      ),
+    );
+  }
+}
