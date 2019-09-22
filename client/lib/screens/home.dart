c4579450ba8b6d8a131e63c75d2a9e3d0ee96351
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-09-22 13:01:36 +0530

    add customize appbar

--- client/lib/screens/home.dart
+++ client/lib/screens/home.dart
@@ -10,12 +10,57 @@ class HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(200.0),
-        child: AppBar(
-          title: Text('Task Manager'),
-          backgroundColor: const Color(0xFF2C9AB0),
+        preferredSize: Size.fromHeight(250.0),
+        child: Container(
+          color: const Color(0xFF2C9AB0),
+          padding: EdgeInsets.only(left: 20, top: 40),
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                height: 250,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 16, top: 56),
+                      child: Text('Hi Kathnrie',
+                        style: TextStyle(fontSize: 30),
+                      )
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-      ),
+        
+      )
+      
     );
   }
 }
+
+
+/* PreferredSize(
+        preferredSize: Size.fromHeight(200.0),
+        child: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 16, top: 56),
+                child: IconButton(
+                  icon: Icon(
+                    Icons.account_circle
+                    ),
+                  onPressed: (){},),
+                ),
+              
+            ],
+          ),
+          backgroundColor: const Color(0xFF2C9AB0),
+        ),
+      ), */
