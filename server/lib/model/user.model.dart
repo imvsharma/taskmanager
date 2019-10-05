193389f62ad6d5e02adfe584b925c2a39ca93842
Vaibhav Sharma <vsvaibhav2016@gmail.com>
2019-10-05 20:06:52 +0530

    user model

---
+++ server/lib/model/user.model.dart
@@ -0,0 +1,26 @@
+import '../server.dart';
+
+class User extends ManagedObject<_User> implements _User {
+
+}
+
+class _User {
+  @primaryKey
+  int id;
+
+  @Column(unique: true, indexed: true)
+  String email;
+
+  @Column(unique: true, indexed: true)
+  String username;
+
+  @Column()
+  String password;
+
+  @Column(unique: false, indexed: true)
+  String firstname;
+
+  @Column(unique: false, indexed: true)
+  String lastname;
+
+}
